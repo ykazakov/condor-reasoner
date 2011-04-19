58721b33f8b65716675312ab6630045c78e51051
fsimancik@yahoo.com <fsimancik@yahoo.com@9b4c283d-3898-f025-9b6e-ec1b7cd1bb2e>
2011-04-19 16:31:36 +0000

    moved to Apache Licence

--- trunk/src/reasoner.cpp
+++ trunk/src/reasoner.cpp
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (c) 2010 Frantisek Simancik
+ * <frantisek.simancik@comlab.ox.ac.uk>, Yevgeny Kazakov
+ * <yevgeny.kazakov@comlab.ox.ac.uk> and University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -587,6 +605,7 @@ int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; i++)  {
 
 	if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+	    cout << "ConDOR r12" << endl;
 	    cout << endl;
 	    cout << "This is an experimental version of the ConDOR reasoner\n"
 		"for classification of SH ontologies. The input file must be\n"
@@ -600,7 +619,7 @@ int main(int argc, char* argv[]) {
 	    cout << "-i  (--input): follow by the input file" << endl;
 	    cout << "-n  (--nooutput): classify the ontology but suppress the output" << endl;
 	    cout << "-o  (--output): follow by the output file" << endl;
-	    cout << "-g  (--goals): follow by a file containing a list of classification goals" << endl;
+//	    cout << "-g  (--goals): follow by a file containing a list of classification goals" << endl;
 //	    cout << "-l  (--log): write performance log into condor.log" << endl;
 	    return 0;
 	}
--- trunk/src/tracker.h
+++ trunk/src/tracker.h
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (c) 2010 Frantisek Simancik
+ * <frantisek.simancik@comlab.ox.ac.uk>, Yevgeny Kazakov
+ * <yevgeny.kazakov@comlab.ox.ac.uk> and University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef TRACKER_H_
 #define TRACKER_H_
 
